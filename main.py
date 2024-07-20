59be22acd7f0a8763a7f83f8b0433238244a94dd
m473 <amakm473@gmail.com>
2024-07-21 00:36:50 +0700

    percabangan lanjut

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 # Percabangan
 jumlah_botol_susu = 173
+jumlah_telur = 2500
+print(f"Jumlah Botol Susu {jumlah_botol_susu} botol")
+print(f"Jumlah Telur {jumlah_telur} butir")
+print()
 
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya dan ternyata uangnya cukup")
-    print("Budi membeli 1 botol susu")
+    if jumlah_telur > 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
+
+print()
+
+print("Budi pulang ke rumah")
+print("Budi menyampaikan hasilnya ke Ibu")
