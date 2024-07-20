70ffb0397ae9beaff237c4a504fc1c043584ef16
m473 <amakm473@gmail.com>
2024-07-21 01:15:53 +0700

    perulangan-for

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,17 @@
+"""
+Program perulangan membaca buku
+"""
+jumlah_buku = 10
+print('Ibu berkata : "Baca semua bukumu"')
+print()
+
+jumlah_buku_yang_sudah_dibaca = 9
+print(f"jumlah Buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+print()
+
+for jumlah_buku_yang_sudah_dibaca in range (1,jumlah_buku_yang_sudah_dibaca+1):
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca ")
+
+print()
+print(f"JUmlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
