a757fc10e66be825c93e387ac9d80b8b9e344c35
m473 <amakm473@gmail.com>
2024-07-21 00:20:54 +0700

    percabangan

---
+++ main.py
@@ -0,0 +1,8 @@
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
