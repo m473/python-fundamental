6e3014d485f1e4e5ea24eb70c18f20b0e85525df
m473 <amakm473@gmail.com>
2024-07-21 01:55:49 +0700

    perulangan-for

--- dasar_sintaksis_perulangan.py
+++ dasar_sintaksis_perulangan.py
@@ -10,8 +10,10 @@ print(f"jumlah Buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
 print()
 
 for jumlah_buku_yang_sudah_dibaca in range (1,jumlah_buku_yang_sudah_dibaca+1):
-    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca ")
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} "
+          f"sudah dibaca ")
 
 print()
 print(f"JUmlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
 
+# tanpa for
